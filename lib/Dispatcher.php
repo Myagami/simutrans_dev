b24c9fafa7c3f581845b0cb113255b455cf5547b
hayate <hayate@Reinforce-Eins.(none)>
2013-08-26 09:05:10 +0900

    テンプレートファイル名とメソッド名の共通化

--- lib/Dispatcher.php
+++ lib/Dispatcher.php
@@ -2,9 +2,26 @@
 class Dispatcher{
 	public function dispatch($Controll,$Method){//振分
 		$Controll_Name = $Controll."_Controller" ;
-		require_once("Controller/".$Controll_Name.".php") ;
-		$Disp = new $Controll_Name($Controll) ;
-		$Disp->$Method() ;
+		$Controll_Path = "Controller/".$Controll_Name.".php" ;
+		
+		try{
+			if(file_exists($Controll_Path)){
+				require_once("Controller/".$Controll_Name.".php") ;
+				$Disp = new $Controll_Name($Controll) ;
+				$Disp->$Method() ;
+				$Disp->display($Method) ;
+			}else{
+				require_once("Controller/Index_Controller.php") ;
+				//throw new Exception("Load Error: Module - {$Controll} ") ;
+				$Disp = new Index_Controller("Index") ;
+				$Disp->Index() ;
+				$Disp->display("Index") ;
+
+			}
+		}catch(Exception $e){
+			echo $e->getMessage() ;
+		}
+		
 	}
 }
 
