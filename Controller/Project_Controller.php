15e900414bd70d925879236992eb6d74fba0df52
hayate <hayate@Reinforce-Eins.(none)>
2013-08-31 20:09:58 +0900

    loading module File_Wrapper / Database_Connect

--- Controller/Project_Controller.php
+++ Controller/Project_Controller.php
@@ -1,10 +1,24 @@
 <?php
-//require_once("Controller/Base_Controller.php") ;
 class Project_Controller extends Base_Controller{
-//class Index_Controller extends Reinforce_Zwei{
-
+	/* 新規プロジェクト */
 	public function Pr_New(){//新規プロジェクト
 		$this->Load_Method("Smarty_Wrapper","assign",array("this_page","Simutrans Develop Tools - New Project ")) ;
+	}
+
+	public function Pr_New_Add(){//新規プロジェクト
+		$this->import_module("URL_Query") ;
+		$this->Load_Method("Smarty_Wrapper","assign",array("this_page","Simutrans Develop Tools - New Project ")) ;
+		$Prj_Prop = $this->Load_Method("URL_Query","get_param",array("Prj")) ;
+		$this->Load_Method("Smarty_Wrapper","assign_array",array("Prj",$Prj_Prop)) ;
+		//ファイル読出
+		$this->import_module_Zwei("File_Wrapper") ;
+		$DBC = $this->Load_Method("File_Wrapper","Load_File","Config/Database_Setting.ini") ;
+		//DB接続
+		echo "<pre>" ;
+		print_r($DBC) ;
+		echo "</pre>" ;
+
+		//$this->import_module("Database_Connect") ;
 	}
 
 	public function Pr_List(){//プロジェクトリスト
@@ -19,6 +33,7 @@ class Project_Controller extends Base_Controller{
 		$this->Load_Method("Smarty_Wrapper","display","Project/".$dis_page.".tpl") ;
 	}
 
+	
 }
 
 ?>
