d4ea53cc0a39b7fef4224494223535b02c7b1952
hayate <hayate@Reinforce-Eins.(none)>
2013-09-01 18:14:51 +0900

    Use Model Case

--- Controller/Project_Controller.php
+++ Controller/Project_Controller.php
@@ -3,6 +3,7 @@ class Project_Controller extends Base_Controller{
 	/* 新規プロジェクト */
 	public function Pr_New(){//新規プロジェクト
 		$this->Load_Method("Smarty_Wrapper","assign",array("this_page","Simutrans Develop Tools - New Project ")) ;
+		$this->display("Pr_New") ;
 	}
 
 	public function Pr_New_Add(){//新規プロジェクト
@@ -10,23 +11,35 @@ class Project_Controller extends Base_Controller{
 		$this->Load_Method("Smarty_Wrapper","assign",array("this_page","Simutrans Develop Tools - New Project ")) ;
 		$Prj_Prop = $this->Load_Method("URL_Query","get_param",array("Prj")) ;
 		$this->Load_Method("Smarty_Wrapper","assign_array",array("Prj",$Prj_Prop)) ;
-		//ファイル読出
-		$this->import_module_Zwei("File_Wrapper") ;
-		$DBC = $this->Load_Method("File_Wrapper","Load_File","Config/Database_Setting.ini") ;
-		//DB接続
-		echo "<pre>" ;
-		print_r($DBC) ;
-		echo "</pre>" ;
 
-		//$this->import_module("Database_Connect") ;
+		//ファイル読出
+		$PRJ_Model = $this->Load_Model("Project_DB") ;
+		$PRJ_Model->add_Project($Prj_Prop) ;
+		/*$this->import_module_Zwei("File_Wrapper") ;
+		$DBC = $this->Load_Method("File_Wrapper","Load_File","Config/Database_Setting.ini") ;
+		//DB用データ構築
+		$PRJ_Data["P_ID"] = date("_ymd_his_").$Prj_Prop["type"] ;
+		$PRJ_Data["P_Name"] = $Prj_Prop["name"] ;
+		$PRJ_Data["Comments"] = $Prj_Prop["disp"] ;
+
+		//DB接続
+		$this->Load_Method_Zwei("Database_Connect","DB_Setting",$DBC["mysql"]) ;
+		$this->Load_Method("Database_Connect","set_table","Project_List") ;
+		$this->Load_Method_Zwei("Database_Connect","insert",$PRJ_Data) ;*/
+		$this->display("Pr_New_Add") ;
+
 	}
 
 	public function Pr_List(){//プロジェクトリスト
 		$this->Load_Method("Smarty_Wrapper","assign",array("this_page","Simutrans Develop Tools")) ;
+		$this->display("Pr_List") ;
+
 	}
 
 	public function Pr_Import(){//インポートでのプロジェクト構成
 		$this->Load_Method("Smarty_Wrapper","assign",array("this_page","Simutrans Develop Tools")) ;
+		$this->display("Pr_Import") ;
+
 	}
 
 	public function Display($dis_page){//画面出力
